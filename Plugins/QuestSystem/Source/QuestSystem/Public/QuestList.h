43f4a2ed36dd282b8de7308776388294b4d94edf
Andrei <burykinmail@gmail.com>
2022-01-20 18:40:22 +0300

    add questlist, questentry

---
+++ Plugins/QuestSystem/Source/QuestSystem/Public/QuestList.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "QuestList.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUESTSYSTEM_API UQuestList : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
